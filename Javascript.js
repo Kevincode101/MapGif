8aa9810762d2e19af20bb00f9ee6023ff7c46e7c
kevin lima <kevin.lima.code@gmail.com>
2017-04-05 22:06:57 +0000

    Updated to know that the funtion worked

---
+++ Javascript.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    var userInput = $("#UserInput").click(function(){
+    alert("The paragraph was clicked.");
+});
+    var location = "#userInput"
+   // console.log( location);
+    var quickgif = $("GeoGif").click()
+
+    //var Result = $('Contents')("helloText");
+});
