a7646c2bead36d2a00ca896e0451031e0908ea99
kevin lima <kevin.lima.code@gmail.com>
2017-04-19 21:49:07 +0000

    Text box can now know what it is reciving

--- Javascript.js
+++ Javascript.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
-    var userInput = $("#UserInput").click(function(){
-    alert("The paragraph was clicked.");
-});
-    var location = "#userInput"
-   // console.log( location);
-    var quickgif = $("GeoGif").click()
+    var userInput = $("#UserInput").click(function() {
+        var text = $("#textinput").val();
+        var location = "#userInput"
+            // console.log( location);
+        var quickgif = $("GeoGif").click()
+
+    });
 
     //var Result = $('Contents')("helloText");
 });
