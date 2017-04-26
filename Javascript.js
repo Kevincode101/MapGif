93a2552f19b7b421b5b4b339933f2f0c57cf218d
kevin lima <kevin.lima.code@gmail.com>
2017-04-26 22:02:58 +0000

    A new picture was added to replace the old one

--- Javascript.js
+++ Javascript.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
     var userInput = $("#UserInput").click(function() {
+        // alert("hello world");
         var text = $("#textinput").val();
         var location = "#userInput"
             // console.log( location);
-        var quickgif = $("GeoGif").click()
-
+        var quickgif = $("#mainimage").attr("src","https://media0.giphy.com/media/tSLapAiNjqjsI/giphy.gif")
     });
 
     //var Result = $('Contents')("helloText");
