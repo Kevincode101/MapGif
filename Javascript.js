fde8e612b3943d301419e7245a2d1de749ae50e2
kevin lima <kevin.lima.code@gmail.com>
2017-05-10 21:53:29 +0000

    Random img is working to the quick gif button

--- Javascript.js
+++ Javascript.js
@@ -4,7 +4,30 @@ $(document).ready(function() {
         var text = $("#textinput").val();
         var location = "#userInput"
             // console.log( location);
-        var quickgif = $("#mainimage").attr("src","https://media0.giphy.com/media/tSLapAiNjqjsI/giphy.gif")
+        $.getJSON(
+
+            "https://api.giphy.com/v1/gifs/search?q=" + text + "&api_key=dc6zaTOxFJmzC",
+
+
+            function(response) {
+                console.log(response);
+                $("#mainimage").attr("src", response.data[1].images.fixed_width_downsampled.url);
+            });
+
+    });
+    var userInput = $("#UserRandom").click(function() {
+       
+        $.getJSON(
+            "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=american+psycho",
+            
+            
+            function(response) {
+                console.log(response);
+                $("#mainimage").attr("src", response.data.image_original_url);
+                // alert(response.data.image_original_url);
+            });
+            
+        // var quickgif = $("#mainimage").attr("src", "https://media0.giphy.com/media/tSLapAiNjqjsI/giphy.gif")
     });
 
     //var Result = $('Contents')("helloText");
